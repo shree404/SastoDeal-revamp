c193c336cb9598d1c216c89f6d11cbe578c7febc
Siddhartha Tiwari <73224879+Sidontheway@users.noreply.github.com>
2023-12-19 15:50:01 +0000

    Project initilized

---
+++ sastodeal-revamp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App m-4 text-cyan-600 font-bold text-2xl">
+      Hello There , This is a test message.
+      </div>
+  );
+}
+
+export default App;
