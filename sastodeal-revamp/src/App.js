52143fdfd8c2235c76f7b1941f7b642256d988ca
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2023-12-22 17:18:16 +0545

    Component of Signup page are  added

--- sastodeal-revamp/src/App.js
+++ sastodeal-revamp/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Button from '@mui/material/Button'
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Button from "@mui/material/Button";
+import SignUpForm from "./Components/Signup/Form";
+
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Button variant='contained'>Hello There</Button>
+      {/* <Navbar/>
+      <Button variant='contained'>Hello There</Button> */}
+    <SignUpForm/>
     </div>
   );
 }
