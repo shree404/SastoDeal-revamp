32687856f197314524f967eea2800effc184e595
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2023-12-23 13:08:41 +0545

    Login Page  added

--- sastodeal-revamp/src/App.js
+++ sastodeal-revamp/src/App.js
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
 
 import './App.css';
-import Category from './Components/Category';
-import Footer from './Components/Footer';
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
-=======
-import "./App.css";
-import Navbar from "./Components/Navbar";
-import Button from "@mui/material/Button";
-import SignUpForm from "./Components/Signup/Form";
-import Or from './Components/Signup/0r';
-import SignupFooter from './Components/Signup/footer.jsx';
-import Signup from "./Components/signup.jsx";
->>>>>>> signup
+import Login from './Pages/Login';
+
+
 
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-
-      <Navbar/>
-      <Home/>
-      <Footer/>
-=======
-      <Signup/>
->>>>>>> signup
+      <Login/>
     </div>
   );
 }
