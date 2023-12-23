c0ad32c5359eacc7cfe63f11b01ff6ccc859c7ef
Siddhartha Tiwari <73224879+Sidontheway@users.noreply.github.com>
2023-12-23 10:24:22 +0000

    Landing Page completion

--- sastodeal-revamp/src/App.js
+++ sastodeal-revamp/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Category from './Components/Category';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
