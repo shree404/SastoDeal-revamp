5d0ac19c579b8c4774ba46d97545a02e73b7e7de
Siddhartha Tiwari <siddharthatiwari37@gmail.com>
2023-12-26 20:12:54 +0545

    Added

--- sastodeal-revamp/src/App.js
+++ sastodeal-revamp/src/App.js
@@ -1,9 +1,5 @@
 
 import "./App.css";
-import ProductDetailedImage from "./Components/Product Description/ProductImage";
-
-import ProductDescription from "./Pages/ProductDescription";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Footer from './Components/Footer';
@@ -13,31 +9,28 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login"
 import Signup from "./Pages/signup"
 import PageNotFound from "./Pages/PageNotFound"
-
+import ProductDescription from "./Pages/ProductDescription"
 
 
 function App() {
   return (
     <div>
 
-      <ProductDescription/>
+      
 
-      {/* <Navbar/>
-      <Home/>
-      <NewArrivals/>
-      <Footer/>
-      <Login/> */}
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={< Home />}/>
           <Route path="/newarrivals" element={<NewArrivals />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={ <Signup />} />
+          <Route path="/product" element={ <ProductDescription />} />
+          
           <Route path = "*" element = {<PageNotFound/>}/>
         </Routes>
         <Footer />
-      </BrowserRouter> */}
+      </BrowserRouter> 
     </div>
   );
 }
